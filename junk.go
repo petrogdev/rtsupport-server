c7f84b3dd257e0b7722537ff62bbc0cdfe0c3463
Petro G <petro.gko@gmail.com>
2018-02-19 20:38:49 -0500

    Modifying main; adding add & subscribe Channel. Trying out goroutine

--- junk.go
+++ junk.go
@@ -3,16 +3,22 @@ package main
 import (
   "encoding/json"
   "fmt"
+  "github.com/mitchellh/mapstructure"
 )
 
 type Message struct {
-  Name string
-  Data interface{}
+  Name string   `json:"name"`
+  Data interface{}  `json:"data"`
+}
+
+type Channel struct {
+  Id   string   `json:"id"`
+  Name string   `json:"name"`
 }
 
 func main() {
-  recRawMsg := []byte(`{name: "channel add", ` +
-    `"data: {"name: "Hardware Support"}}`)
+  recRawMsg := []byte(`{"name": "channel add", ` +
+    `"data": {"name": "Hardware Support"}}`)
 
   var recMessage Message
   err := json.Unmarshal(recRawMsg, &recMessage)
@@ -20,9 +26,40 @@ func main() {
     fmt.Println(err)
     return
   }
-  fmt.Fprintf("%#v\n", recMessage)
+  fmt.Printf("%#v\n", recMessage)
+
+  if recMessage.Name == "channel add" {
+    channel, err := addChannel(recMessage.Data)
+    var sendMessage Message
+    sendMessage.Name = "channel add"
+    sendMessage.Data = channel
+    sendRawMsg, err := json.Marshal(sendMessage)
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+    fmt.Printf(string(sendRawMsg))
+  }
 }
 
+func addChannel(data interface{}) (Channel, error) {
+  var channel Channel
+  // channelMap := data.(map[string]interface{})
+  // channel.Name = channelMap["name"].(string)
+  err := mapstructure.Decode(data, &channel)
+  if err != nil {
+    return channel, err
+  }
+  channel.Id = "1"
+
+  fmt.Printf("%#v\n", channel)
+  return channel, nil
+}
+
+
+// func printInGo(msg) {
+//   return fmt.Printf("%#v\n", msg)
+// }
 
 // type Speak interface {
 //   Speak()
