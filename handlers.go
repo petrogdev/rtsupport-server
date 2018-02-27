5bda4a0add81b5789239019579a844c13606fef7
Petro G <petro.gko@gmail.com>
2018-02-26 23:19:02 -0500

    Adding stop routines to eliminate go leaks

--- handlers.go
+++ handlers.go
@@ -3,13 +3,18 @@ package main
 import (
   "github.com/mitchellh/mapstructure"
   r "github.com/dancannon/gorethink"
+  "fmt"
+)
+
+const (
+  ChannelStop = iota
+  UserStop
+  MessageStop
 )
 
 func addChannel(client *Client, data interface{}) {
   var channel Channel
-
   err := mapstructure.Decode(data, &channel)
-
   if err != nil {
     client.send <- Message{"error", err.Error()}
     return
@@ -24,4 +29,44 @@ func addChannel(client *Client, data interface{}) {
       client.send <- Message{"error", err.Error()}
     }
   }()
+}
+
+func subscribeChannel(client *Client, data interface{}) {
+  stop := client.NewStopChannel(ChannelStop)
+  result := make(chan r.ChangeResponse)
+
+  cursor, err := r.Table("channel").
+    Changes(r.ChangesOpts{IncludeInitial: true}).
+    Run(client.session)
+
+  if err != nil {
+    client.send <- Message{"error", err.Error()}
+    return
+  }
+
+  go func() {
+     var change r.ChangeResponse
+     for cursor.Next(&change) {
+       result <- change
+     }
+  }()
+
+  go func() {
+    for {
+      select {
+        case <- stop:
+          cursor.Close()
+          return
+        case change := <- result:
+          if change.NewValue != nil && change.OldValue == nil {
+            client.send <- Message{"channel add", change.NewValue}
+            fmt.Println("sent channel add msg")
+          }
+      }
+    }
+  }()
+}
+
+func unsubscribeChannel(client *Client, data interface{}) {
+  client.StopForKey(ChannelStop)
 }
